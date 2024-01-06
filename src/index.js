0c4abecfdcee8cd4760ae77c2c8a6e5fd60cfacb
sunny7657 <ksvetlana466@gmail.com>
2024-01-06 15:58:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+const el1 = <span>hey</span>;
+const el2 = <span>world</span>;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const jsxEl = (
+  <div>
+    {el1}
+    {el2}
+  </div>
 );
+console.log(jsxEl);
+
+// ReactDOM.render(element, document.querySelector('#root'));
